6d2f7ce957f930b451cba76698d9e418f7f8cf5c
NoPlagiarism <37241775+NoPlagiarism@users.noreply.github.com>
2024-11-20 21:26:07 +0500

    chores: remove some debug lines

--- src/favicon.py
+++ src/favicon.py
@@ -50,9 +50,7 @@ class FaviconManager:
     
     @staticmethod
     def get_fav_path_in_folder(dom: str | httpx.URL, path: str) -> t.Optional[str]:
-        logger.debug(f"{dom} -> {BasicFaviconProvider.simplify_domain(BasicFaviconProvider.get_domain(dom))}")
         fav_path = os.path.join(path, BasicFaviconProvider.simplify_domain(BasicFaviconProvider.get_domain(dom)) + ".png")
-        logger.debug(f"{BasicFaviconProvider.simplify_domain(BasicFaviconProvider.get_domain(dom))} -> {fav_path}")
         if os.path.exists(fav_path):
             return fav_path
     
