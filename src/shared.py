5131eff021200c0099bc9431a2beb4c90290614e
NoPlagiarism <37241775+NoPlagiarism@users.noreply.github.com>
2025-09-13 15:59:32 +0500

    fix: support non-portable FL (resolve #7)

--- src/shared.py
+++ src/shared.py
@@ -17,8 +17,20 @@ SETTINGS_TYPE = t.TypedDict('Settings', {
 })
 
 FLOW_PROGRAM_DIRECTORY = os.environ.get("FLOW_PROGRAM_DIRECTORY")
-FL_SETTINGS_FILE = os.path.join(FLOW_PROGRAM_DIRECTORY, "UserData", "Settings", "Settings.json") if FLOW_PROGRAM_DIRECTORY else None
-PLUGIN_SETTINGS_DIRECTORY = os.path.join(FLOW_PROGRAM_DIRECTORY, "UserData", "Settings", "Plugins", "ShikiFlow") if FLOW_PROGRAM_DIRECTORY else None
+FLOW_APPLICATION_DIRECTORY = os.environ.get("FLOW_APPLICATION_DIRECTORY")
+
+def is_portable():
+    portable_data_path = os.path.join(FLOW_APPLICATION_DIRECTORY, "UserData")
+    portable_data_deletion_indicator = os.path.join(FLOW_APPLICATION_DIRECTORY, "UserData", ".dead")
+    return os.path.exists(portable_data_path) and not os.path.exists(portable_data_deletion_indicator)
+
+if is_portable():
+    DATA_DIR = os.path.join(FLOW_PROGRAM_DIRECTORY, "UserData")
+else:
+    DATA_DIR = os.path.expandvars("%APPDATA%\\FlowLauncher")
+
+FL_SETTINGS_FILE = os.path.join(DATA_DIR, "Settings", "Settings.json") if FLOW_PROGRAM_DIRECTORY else None
+PLUGIN_SETTINGS_DIRECTORY = os.path.join(DATA_DIR, "Settings", "Plugins", "ShikiFlow") if FLOW_PROGRAM_DIRECTORY else None
 SETTINGS_FILE = os.path.join(PLUGIN_SETTINGS_DIRECTORY, "Settings.json") if PLUGIN_SETTINGS_DIRECTORY else None
 OSETTINGS_FILE = os.path.join(PLUGIN_SETTINGS_DIRECTORY, "osettings.json") if PLUGIN_SETTINGS_DIRECTORY else None
 PLUGIN_CACHE_FOLDER = os.path.join(PLUGIN_SETTINGS_DIRECTORY, "Cache") if PLUGIN_SETTINGS_DIRECTORY else None
